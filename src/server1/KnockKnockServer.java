b145a91deee317c7d167be66c9987dae4563ca25
DraganDjuric94 <juhuudd@gmail.com>
2017-03-24 12:02:40 +0100

    aaaaaa

---
+++ src/server1/KnockKnockServer.java
@@ -0,0 +1,57 @@
+/*
+* To change this license header, choose License Headers in Project Properties.
+* To change this template file, choose Tools | Templates
+* and open the template in the editor.
+*/
+package server1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ *
+ * @author Juhu
+ */
+
+public class KnockKnockServer {
+    public static void main(String[] args) throws IOException {
+        
+        ServerSocket serverSocket = null;
+        try {
+            serverSocket = new ServerSocket(4444);
+        } catch (IOException e) {
+            System.err.println("Could not listen on port: 4444.");
+            System.exit(1);
+        }
+        
+        Socket clientSocket = null;
+        try {
+            clientSocket = serverSocket.accept();
+        } catch (IOException e) {
+            System.err.println("Accept failed.");
+            System.exit(1);
+        }
+        
+        PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(
+                        clientSocket.getInputStream()));
+        String inputLine, outputLine = null;
+
+        
+        while ((inputLine = in.readLine()) != null) {
+            out.println(outputLine);
+            if (outputLine.equals("Bye."))
+                break;
+        }
+        out.close();
+        in.close();
+        clientSocket.close();
+        serverSocket.close();
+    }
+}
+
